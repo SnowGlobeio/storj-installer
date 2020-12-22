bc8ba57a02d5377c412e37fecb09a15e13988a17
LabSeb <35532325+SnowGlobeio@users.noreply.github.com>
2020-12-22 16:16:35 +0100

    Update storj-installer.sh
    REmove swap file if 4Go Ram

--- storj-installer.sh
+++ storj-installer.sh
@@ -8,11 +8,21 @@ else
   echo "ARCH: 32-bit"
   $ARCH = "32";
 fi
+#Detect memory size
+$totalm=$(free -m | awk '/^Mem:/{print $2}') ;
 
 echo "Storj node installer"
 echo "1° - Raspberry PI update...."
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo rpi-update
+
+if ($totalm > 4000);then
+  echo "Disable Swap file on SDCARD"
+  sudo dphys-swapfile swapoff
+  sudo dphys-swapfile uninstall
+  sudo update-rc.d dphys-swapfile remove
+  sudo apt purge dphys-swapfile
+fi
 
 echo ""
 echo "2° - create Storj node directory"
@@ -37,7 +47,7 @@ echo "4° - Docker"
 echo "4°1 - prepare..."
 sudo echo "cgroup_enable=memory cgroup_memory=1 swapaccount=1" >> /boot/cmdline.txt
 echo "4°2 - Download....."
-curl -sSL https://get.docker.com | sh
+curl -sSL https://get.docker.com | sudo sh
 
 echo "4°3 - Download Storj docker file"
 sudo docker pull storjlabs/storagenode:beta
